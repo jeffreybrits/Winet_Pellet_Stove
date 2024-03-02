eb5fccadaa65d78f52d42462aa8cc958bb17b315
jeffreybrits <55175145+jeffreybrits@users.noreply.github.com>
2024-03-02 16:27:55 +0100

    Update toggle_power_on.sh

--- shell/stove/toggle_power_on.sh
+++ shell/stove/toggle_power_on.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# This script sends a POST request to the specified host with custom headers and data.
+# This script sends a POST request to the WINET Pellet Stove Module
+# The module will return a status to indicate if the call was succesfull or not
+# This command writes the result in a response file which i use in a file sensor.
+# Rememeber to change IPADDRESS with you IP Address of the WINET Module
 
 rm /config/shell/stove/response_toggle_power_on.txt
 echo "working" > /config/shell/stove/response_toggle_power_on.txt
