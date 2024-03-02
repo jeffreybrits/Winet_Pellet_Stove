5905caafb668c3c1b10eae406d23ea0ac4dd7177
jeffreybrits <55175145+jeffreybrits@users.noreply.github.com>
2024-03-02 16:24:59 +0100

    Create power3.sh

---
+++ shell/stove/power3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This script sends a POST request to the WINET Pellet Stove Module
+# The module will return a status to indicate if the call was succesfull or not
+# This command writes the result in a response file which i use in a file sensor.
+# Rememeber to change IPADDRESS with you IP Address of the WINET Module
+
+rm /config/shell/stove/response_power2.txt
+echo "working" > /config/shell/stove/response_power3.txt
+sleep 5
+
+curl -i -s -k -X $'POST' \
+    --data-binary $'key=002&memory=1&regId=51&value=3' \
+    $'http://192.168.1.110/ajax/set-register' > /config/shell/stove/response_power3.txt
