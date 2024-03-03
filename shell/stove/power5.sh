53a9c588433b3756fc9d9d455495be77aceb1fd9
jeffreybrits <55175145+jeffreybrits@users.noreply.github.com>
2024-03-03 10:15:06 +0100

    Update power5.sh

--- shell/stove/power5.sh
+++ shell/stove/power5.sh
@@ -11,4 +11,4 @@ sleep 5
 
 curl -i -s -k -X $'POST' \
     --data-binary $'key=002&memory=1&regId=51&value=5' \
-    $'http://192.168.1.110/ajax/set-register' > /config/shell/stove/response_power5.txt
+    $'http://IPADDRESS/ajax/set-register' > /config/shell/stove/response_power5.txt
